2d0622e4acdb5ef6e0d9c1e577fcda19521ea33a
jeppeman <jesperaamann@gmail.com>
2022-05-28 08:20:59 +0200

    Initial

---
+++ precache-manifest.e11c116b9489c647c2b6c1eaa49f8400.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce50face7c117166bf1dc81bcd533620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b66363c07e0d499d140",
+    "url": "/static/css/main.46875f0a.chunk.css"
+  },
+  {
+    "revision": "a3c9a8818ac62b54ca1c",
+    "url": "/static/js/2.f027abe4.chunk.js"
+  },
+  {
+    "revision": "af3faa7d6a08dd08000696fa6bddc6dc",
+    "url": "/static/js/2.f027abe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b66363c07e0d499d140",
+    "url": "/static/js/main.08e4ae49.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "680953bfc99c9053ed4729c4df243c1b",
+    "url": "/static/media/amazon.680953bf.gif"
+  },
+  {
+    "revision": "e2d3f4a8ccd868d0c4929b000126c85c",
+    "url": "/static/media/android_studio.e2d3f4a8.svg"
+  },
+  {
+    "revision": "763bf138ff7401c9d5185e6d2218be90",
+    "url": "/static/media/apk_from_bundle.763bf138.png"
+  },
+  {
+    "revision": "bc667871dc48404e05ff596abc9ec9a9",
+    "url": "/static/media/cover_image.bc667871.png"
+  },
+  {
+    "revision": "9f33017e04d22d4d2406a5f52b5f8ae0",
+    "url": "/static/media/flow_diagram.9f33017e.png"
+  },
+  {
+    "revision": "36bb7afaab9d9ad8598b7881f869081c",
+    "url": "/static/media/galaxy.36bb7afa.gif"
+  },
+  {
+    "revision": "1f8beabd990baa19724a71310bbad5c3",
+    "url": "/static/media/gradle.1f8beabd.svg"
+  },
+  {
+    "revision": "58134ee8301433182124f54a28831e34",
+    "url": "/static/media/huawei.58134ee8.gif"
+  },
+  {
+    "revision": "87ade2c4617c41a70f46a0e3e8b7c9ff",
+    "url": "/static/media/java.87ade2c4.svg"
+  },
+  {
+    "revision": "031f4618c896fa47b19c29a901253602",
+    "url": "/static/media/menu_closed.031f4618.svg"
+  },
+  {
+    "revision": "ff75433886d3588654585b6b7f9338b4",
+    "url": "/static/media/menu_opened.ff754338.svg"
+  },
+  {
+    "revision": "c5b287b77e99a8280095d72b6f965cf1",
+    "url": "/static/media/plugin_in_studio.c5b287b7.png"
+  },
+  {
+    "revision": "a0f21137309382140d732fc0b55fc311",
+    "url": "/static/media/tool_window.a0f21137.png"
+  },
+  {
+    "revision": "31294fd139c975face1a1e36e6568330",
+    "url": "/static/media/top_menu.31294fd1.png"
+  },
+  {
+    "revision": "b6be3a2560dbf84d483a67ac7923c6fc",
+    "url": "/static/media/upload_log.b6be3a25.jpg"
+  }
+]);
